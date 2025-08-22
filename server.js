7cca1ff81128207cc7ef5c11be5db4fdbd3aa6b3
abdulSattar <sattargkl4@gmail.com>
2025-08-22 15:06:11 +0100

    url chnage

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 import { errorMiddleware } from "./middlewares/errorMiddleware.js";
 app.use(
   cors({
-    origin: ["https://chatapp-dusky-delta.vercel.app"],
+    origin: [process.env.CLIENT_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
